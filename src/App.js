d43a2406a35642516ef07cd57ce6fb3b1ded829a
Felipe Augusto <felipenax@gmail.com>
2023-03-06 21:31:55 -0300

    Configuracao e Teste do Ambiente Docker

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Felipe conseguiu <code>src/App.js</code> chegar até aqui and save to reload.
         </p>
         <a
           className="App-link"
